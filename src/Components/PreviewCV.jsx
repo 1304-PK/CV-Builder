cb6108e74019671e9f5053ee680c742099ab94ba
1304-PK <pushkark1304@gmail.com>
2025-07-10 17:03:53 +0530

    add skill, work experience and project section to cv

--- src/Components/PreviewCV.jsx
+++ src/Components/PreviewCV.jsx
@@ -1,4 +1,4 @@
-function PreviewCV({personalForm, educationForm}){
+function PreviewCV({personalForm, educationForm, projectForm, skillForm, workForm}){
     return(
         <div className="preview-cv">
             <div id="personal-info">
@@ -6,15 +6,44 @@ function PreviewCV({personalForm, educationForm}){
                 <p>{personalForm.location}|{personalForm.phone}|{personalForm.email}</p>
             </div>
             <div id="education-info">
+                <h1>Education</h1>
+                <hr />
                 <div>
-                    <p>{educationForm.institution}|{educationForm.location}</p>
+                    <p>{educationForm.institution},{educationForm.location}</p>
                     <p>{educationForm.degree}</p>
                 </div>
                 <div>
                     <p>{educationForm.start_year}-{educationForm.end_year}</p>
+                    <p>CGPA: {educationForm.cgpa}</p>
                 </div>
-                <p>{educationForm.description}</p>
-            </div>  
+            </div>
+            <div id="project-info">
+                <h1>Projects</h1>
+                <hr />
+                <div>
+                <p>{projectForm.name} | {projectForm.tech_stack}</p>
+                <p>{projectForm.date}</p>
+                </div>
+                <p>{projectForm.description}</p>
+            </div>
+            <div id="work-info">
+                <h1>Experience</h1>
+                <hr />
+                <div>
+                    <p>{workForm.organization}</p>
+                    <p>{workForm.job_title}</p>
+                </div>
+                <div>
+                    <p>{workForm.start_date} - {workForm.end_date}</p>
+                    <p>{workForm.location}</p>
+                </div>
+                <p>{workForm.description}</p>
+            </div>
+            <div id="skill-info">
+                <h1>Technical Skills</h1>
+                <hr />
+                <p>{skillForm.title}: {skillForm.skill}</p>
+            </div>
         </div>
     )
 }
