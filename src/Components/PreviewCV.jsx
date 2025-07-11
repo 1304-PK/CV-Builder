27616634a2977a9b6fc9e72cf4664eab936d27ee
1304-PK <pushkark1304@gmail.com>
2025-07-11 17:56:06 +0530

    minor

--- src/Components/PreviewCV.jsx
+++ src/Components/PreviewCV.jsx
@@ -1,4 +1,4 @@
-function PreviewCV({ personalForm, educationForm, projectForm, skillForm, workForm }) {
+function PreviewCV({ personalForm, educationForm, projects, skills, experiences }) {
     return (
         <div className="preview-cv">
             <div id="personal-info">
@@ -20,31 +20,47 @@ function PreviewCV({ personalForm, educationForm, projectForm, skillForm, workFo
             </div>
             <div id="project-info">
                 <h4>PROJECTS</h4>
-                <div className="split-horizontally">
-                    <p><span>{projectForm.name || "Marauder's Map Enhancement"}</span> | {projectForm.tech_stack || 'Revelio, Mischief Managed, Charms, Advanced Mapmaking'}</p>
-                    <p><span>{projectForm.date || "May 1996"}</span></p>
+                {projects.map(p => {
+                    return (
+                        <>
+                        <div className="split-horizontally">
+                    <p><span>{p.name || "Marauder's Map Enhancement"}</span> | {p.tech_stack || 'Revelio, Mischief Managed, Charms, Advanced Mapmaking'}</p>
+                    <p><span>{p.date || "May 1996"}</span></p>
                 </div>
-                <p>{projectForm.description || "Improved the legendary Marauder’s Map with real-time location tracking, ghost detection, and unauthorized intruder alerts. Integrated magical encryption to prevent misuse."}</p>
+                <p>{p.description || "Improved the legendary Marauder's Map with real-time location tracking, ghost detection, and unauthorized intruder alerts. Integrated magical encryption to prevent misuse."}</p>
+                </>
+                    )
+                })}
             </div>
             <div id="work-info">
-                <h4>EXPERIENCE</h4>
+            <h4>EXPERIENCE</h4>
+                {experiences.map(e => {
+                    return (
+                        <>
                 <div className="split-horizontally">
                     <div>
-                        <p><span>{workForm.organization || 'Department of Magical Accidents and Catastrophes'}</span></p>
-                        <p>{workForm.job_title || 'Auror Trainee'}</p>
+                        <p><span>{e.organization || 'Department of Magical Accidents and Catastrophes'}</span></p>
+                        <p>{e.job_title || 'Auror Trainee'}</p>
                     </div>
                     <div className="right-section">
-                        <p><span>{workForm.start_date || 'June 2024'} - {workForm.end_date || 'May 2025'}</span></p>
-                        <p>{workForm.location || 'Ministry of Magic'}</p>
+                        <p><span>{e.start_date || 'June 2024'} - {e.end_date || 'May 2025'}</span></p>
+                        <p>{e.location || 'Ministry of Magic'}</p>
                     </div>
                 </div>
-                <p>{workForm.description || `• Led specialized task force in neutralizing 15+ dark magic incidents across Greater London area
+                <p>{e.description || `• Led specialized task force in neutralizing 15+ dark magic incidents across Greater London area
                     • Collaborated with Senior Aurors to develop new protective ward systems, improving department safety protocols by 40%
                     • Mentored 8 junior wizards in advanced defensive spell casting and emergency response procedures`}</p>
+                        </>
+                    )
+                })}
             </div>
             <div id="skill-info">
-                <h4>TECHNICAL SKILLS</h4>
-                <p><span>{skillForm.title || 'Magical Skills'}:</span> {skillForm.skill || 'Advanced Patronus Casting, Parseltongue Communication, Broomstick Flight Proficiency, Time-Turner Navigation'}</p>
+            <h4>TECHNICAL SKILLS</h4>
+            {skills.map(s => {
+                return (
+                        <p><span>{s.title || 'Magical Skills'}:</span> {s.skill || 'Advanced Patronus Casting, Parseltongue Communication, Broomstick Flight Proficiency, Time-Turner Navigation'}</p>
+                )
+            })}
             </div>
         </div>
     )
