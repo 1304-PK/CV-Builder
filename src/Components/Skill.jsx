aec2eba6275e1e0e16dbe8aa46ff05bb3398f5cb
1304-PK <pushkark1304@gmail.com>
2025-07-11 17:54:35 +0530

    added add skill and remove skill button

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -7,13 +7,27 @@ function GenForm(p){
     )
 }
 
-function Skill({skillForm, updateSkillForm}){
+function Skill({skillForm, updateSkillForm, addSkill, skills, removeSkill}){
     return(
     <div>
+        <div className="info-list">
+            {skills.map((s, i) => {
+                return(
+                    <div className="info-list-content">
+                        <p>{s.title}</p>
+                        <button onClick={() => {
+                            const arr = skills.filter(item => item.title!==s.title)
+                            removeSkill(arr)
+                        }}>❌</button>
+                    </div>
+                )
+            })}
+        </div>
         <form className="form">
             <GenForm id="title" label="Title" type="text" value={skillForm.title} onChange={updateSkillForm} />
             <GenForm id="skill" label="Skills" type="text" value={skillForm.skill} onChange={updateSkillForm} />
         </form>
+        <button className='add-button' onClick={addSkill}>Add</button>
     </div>
     )
 }
