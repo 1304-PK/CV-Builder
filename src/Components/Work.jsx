b645c3fab504488365ae24639420ef910b2335e9
1304-PK <pushkark1304@gmail.com>
2025-07-11 17:54:52 +0530

    added add experience and remove experience button

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,24 +1,38 @@
-function GenForm(p){
-    return(
+function GenForm(p) {
+    return (
         <div>
-        <label htmlFor={p.id}>{p.label}</label>
-        <input type={p.type} id={p.id} value={p.value} onChange={p.onChange}/>
+            <label htmlFor={p.id}>{p.label}</label>
+            <input type={p.type} id={p.id} value={p.value} onChange={p.onChange} />
         </div>
     )
 }
 
-function Work({workForm, updateWorkForm}){
-    return(
-    <div className="form">
-        <form>
-            <GenForm id="organization" label="Organization:" type="text" value={workForm.oganization} onChange={updateWorkForm} />
-            <GenForm id="location" label="Location:" type="text" value={workForm.location} onChange={updateWorkForm} />
-            <GenForm id="job_title" label="Job Title:" type="text" value={workForm.job_title} onChange={updateWorkForm} />
-            <GenForm id="start_date" label="Start Date:" type="text" value={workForm.start_date} onChange={updateWorkForm} />
-            <GenForm id="end_date" label="End Date:" type="text" value={workForm.end_date} onChange={updateWorkForm} />
-            <GenForm id="description" label="Responsibilities/Achievements:" type="text" value={workForm.description} onChange={updateWorkForm} />
-        </form>
-    </div>
+function Work({ workForm, updateWorkForm, addExperience, experiences, removeExperience }) {
+    return (
+        <div>
+            <div className="info-list">
+                {experiences.map(s => {
+                    return (
+                        <div className="info-list-content">
+                            <p>{s.organization}</p>
+                            <button onClick={() => {
+                                const arr = experiences.filter(item => item.organization!==s.organization)
+                                removeExperience(arr)
+                            }}>❌</button>
+                        </div>
+                    )
+                })}
+            </div>
+            <form className="form">
+                <GenForm id="organization" label="Organization:" type="text" value={workForm.oganization} onChange={updateWorkForm} />
+                <GenForm id="location" label="Location:" type="text" value={workForm.location} onChange={updateWorkForm} />
+                <GenForm id="job_title" label="Job Title:" type="text" value={workForm.job_title} onChange={updateWorkForm} />
+                <GenForm id="start_date" label="Start Date:" type="text" value={workForm.start_date} onChange={updateWorkForm} />
+                <GenForm id="end_date" label="End Date:" type="text" value={workForm.end_date} onChange={updateWorkForm} />
+                <GenForm id="description" label="Responsibilities/Achievements:" type="text" value={workForm.description} onChange={updateWorkForm} />
+            </form>
+            <button className='add-button' onClick={addExperience}>Add</button>
+        </div>
     )
 }
 
