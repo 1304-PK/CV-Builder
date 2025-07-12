666b25e5d6b8f54c2c0f96b876928c7c4568b8aa
1304-PK <pushkark1304@gmail.com>
2025-07-13 01:06:04 +0530

    minor

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -11,12 +11,12 @@ function Education({ educationForm, updateEducationForm }) {
     return (
         <div>
             <form className="form">
-                <GenForm id="institution" label="Institution" type="text" value={educationForm.institution} onChange={updateEducationForm} placeholder={'e.g. Harvard University'}/>
-                <GenForm id="location" label="Location" type="text" value={educationForm.location} onChange={updateEducationForm} placeholder={'e.g. Cambridge'}/>
-                <GenForm id="degree" label="Degree" type="text" value={educationForm.degree} onChange={updateEducationForm} placeholder={'e.g. Bachelor of Arts'}/>
-                <GenForm id="start_year" label="Start Year" type="text" value={educationForm.start_year} onChange={updateEducationForm} placeholder={'e.g. June 2020'}/>
-                <GenForm id="end_year" label="End Year" type="text" value={educationForm.end_year} onChange={updateEducationForm} placeholder={'e.g. April 2024'}/>
-                <GenForm id="cgpa" label="CGPA" type="number" value={educationForm.cgpa} onChange={updateEducationForm} placeholder={'e.g. 8.7'}/>
+                <GenForm id="institution" label="Institution" type="text" value={educationForm.institution} onChange={updateEducationForm} placeholder={'Harvard University'}/>
+                <GenForm id="location" label="Location" type="text" value={educationForm.location} onChange={updateEducationForm} placeholder={'Cambridge'}/>
+                <GenForm id="degree" label="Degree" type="text" value={educationForm.degree} onChange={updateEducationForm} placeholder={'Bachelor of Arts'}/>
+                <GenForm id="start_year" label="Start Year" type="text" value={educationForm.start_year} onChange={updateEducationForm} placeholder={'June 2020'}/>
+                <GenForm id="end_year" label="End Year" type="text" value={educationForm.end_year} onChange={updateEducationForm} placeholder={'April 2024'}/>
+                <GenForm id="cgpa" label="CGPA" type="number" value={educationForm.cgpa} onChange={updateEducationForm} placeholder={'8.7'}/>
             </form>
         </div>
     )
